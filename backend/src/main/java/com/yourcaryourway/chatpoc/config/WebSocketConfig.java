d40850a3a7608f4dfadf16064245434ab958feb1
Aleks Harisov <160621683+aharisov@users.noreply.github.com>
2026-08-27 17:38:24 +0200

    feat: configure WebSocket endpoint

---
+++ backend/src/main/java/com/yourcaryourway/chatpoc/config/WebSocketConfig.java
@@ -0,0 +1,25 @@
+package com.yourcaryourway.chatpoc.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+import com.yourcaryourway.chatpoc.handler.ChatWebSocketHandler;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    private final ChatWebSocketHandler chatWebSocketHandler;
+
+    public WebSocketConfig(ChatWebSocketHandler chatWebSocketHandler) {
+        this.chatWebSocketHandler = chatWebSocketHandler;
+    }
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(chatWebSocketHandler, "/ws")
+                .setAllowedOrigins("http://localhost:4200");
+    }
+}
---
+++ backend/src/main/java/com/yourcaryourway/chatpoc/handler/ChatWebSocketHandler.java
@@ -0,0 +1,9 @@
+package com.yourcaryourway.chatpoc.handler;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+@Component
+public class ChatWebSocketHandler extends TextWebSocketHandler {
+
+}
